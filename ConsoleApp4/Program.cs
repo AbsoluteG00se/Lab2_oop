eadd534b9434f1b575f1a3c32170b8984f2ecee7
Евгений Абрамович < @DESKTOP-LLJ375A>
2023-04-06 21:56:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Laba_2
+{
+    abstract class Currency
+    {
+        public int value;
+        public string type;
+        public int conversionRate; 
+
+        public abstract double convertToRubles();
+        public abstract void display();
+
+        virtual display()
+        {
+
+        }
+    }
+
+    class Dollar : Currency
+    {
+        public override double convertToRubles()
+        {
+            return value * conversionRate;
+        }
+
+        public override void display(
+            
+            )
+        {
+            Console.WriteLine($"Type of currency: {type}");
+            Console.WriteLine($"Amount of money: {value}");
+            Console.WriteLine($"Value in rubles after conversion: {this.convertToRubles()}");
+        }
+    }
+
+    class Euro : Currency
+    {
+        public override double convertToRubles()
+        {
+            return value * (conversionRate / 0.85);
+        }
+
+        public override void display()
+        {
+            Console.WriteLine($"Type of currency: {type}");
+            Console.WriteLine($"Amount of money: {value}");
+            Console.WriteLine($"Value in rubles after conversion: {this.convertToRubles()}");
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Currency myDollar = new Dollar { value = 100, type = "Dollar", conversionRate = 75 };
+            Console.WriteLine("Object of Dollar:");
+            myDollar.display();
+
+            Currency myEuro = new Euro { value = 50, type = "Euro", conversionRate = 85 };
+            Console.WriteLine("Object of Euro:");
+            myEuro.display();
+        }
+    }
+}
